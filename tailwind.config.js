2f257ebe01df77433862a46bb3c85da71794719e
gi0210lee <gi0210lee@gmail.com>
2022-04-28 10:57:07 +0900

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  // purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      fontWeight: {},
+    },
   },
   plugins: [],
 }
